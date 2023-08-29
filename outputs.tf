dd8d3a82d1b9ea5df68fbe20d16a2f20ce48a098
Nicolas Nunez <nicolasnunezromay@gmail.com>
2023-08-29 17:02:19 -0500

    Output public ips

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
-# output "ec2_instance_public_ips" {
-#   description = "Public IP addresses of EC2 instances"
-#   value       = module.ec2_instances[*].public_ip
-# }
+output "ec2_instance_public_ips" {
+  description = "Public IP addresses of EC2 instances"
+  value       = module.ec2_instances[*].public_ip
+}
 
-  output "web_servers_key_pair_out" {
-    description = "Key Pair for Web Servers"
-    value = data.aws_key_pair.web_servers_key
-  }
+output "web_servers_key_pair_out" {
+  description = "Key Pair for Web Servers"
+  value = data.aws_key_pair.web_servers_key
+}
